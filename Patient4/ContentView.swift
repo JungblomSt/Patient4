5cde7db3d7128461bcf5308e33479c1704ea555c
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2026-04-17 14:45:44 +0200

    Initial Commit

---
+++ Patient4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Patient4
+//
+//  Created by Stina Thun on 2026-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Patient4/Patient4App.swift
@@ -0,0 +1,17 @@
+//
+//  Patient4App.swift
+//  Patient4
+//
+//  Created by Stina Thun on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct Patient4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
